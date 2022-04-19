5fcfb1f96f7d6dc5274648e33282ca1d1eb02cf0
Hi ! Shakya <hishakya@His-MacBook-Air.local>
2022-04-19 19:36:24 +0530

    rolldex initialize app

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import Card  from "../card/card.component"
+import './card-list.styles.css'
+const CardList = () => {
+    return(
+        <div className="card-list">   
+             <Card/>
+             <Card/>
+        </div>
+        
+    )
+}
+
+export default CardList
---
+++ src/components/search/search.component.jsx
@@ -0,0 +1,11 @@
+const Search = () => {
+    return (
+    
+        <input
+        className="search-box"
+        type="text"
+        placeholder="Seach Monstersa"/>
+    )
+}
+
+export default Search
